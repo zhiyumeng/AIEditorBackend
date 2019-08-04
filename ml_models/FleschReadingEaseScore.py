0b74796c446fad43d1750b7f3ee9f9a0c3569797
fyyc <zhiyumengxiang@outlook.com>
2019-08-04 14:22:45 +0800

    update 句子可读性

--- ml_models/FleschReadingEaseScore.py
+++ ml_models/FleschReadingEaseScore.py
@@ -10,20 +10,21 @@ import pandas as pd
 # import string
 import spacy
 
+
 def syllables(word):
     '''
     利用规则判断单词的音节数,
     输入：单词，
     输出：数字
     '''
-    #referred from stackoverflow.com/questions/14541303/count-the-number-of-syllables-in-a-word
+    # referred from stackoverflow.com/questions/14541303/count-the-number-of-syllables-in-a-word
     count = 0
     vowels = 'aeiouy'
     word = word.lower()
     if word[0] in vowels:
         count += 1
-    for index in range(1,len(word)):
-        if word[index] in vowels and word[index-1] not in vowels:
+    for index in range(1, len(word)):
+        if word[index] in vowels and word[index - 1] not in vowels:
             count += 1
     if word.endswith('e'):
         count -= 1
@@ -32,6 +33,7 @@ def syllables(word):
     if count == 0:
         count += 1
     return count
+
 
 def cmusyl(word):
     '''
@@ -44,8 +46,9 @@ def cmusyl(word):
     try:
         return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
     except KeyError:
-        #if word not found in cmudict
+        # if word not found in cmudict
         return [syllables(word)]
+
 
 def fresDescr(score):
     '''
@@ -54,17 +57,20 @@ def fresDescr(score):
     输出：描述语句
     '''
     score_range = {'Very easy to read. Easily understood by an average 11-year-old student.': 90,
-                   'Easy to read. Conversational English for consumers.': 80, 
-                   'Fairly easy to read.':70,
-                   'Plain English. Easily understood by 13- to 15-year-old students.': 60, 
-                   'Fairly difficult to read.': 50, 
+                   'Easy to read. Conversational English for consumers.': 80,
+                   'Fairly easy to read.': 70,
+                   'Plain English. Easily understood by 13- to 15-year-old students.': 60,
+                   'Fairly difficult to read.': 50,
                    'Difficult to read.': 30,
                    'Very difficult to read. Best understood by university graduates.': -1000}
     for k, v in score_range.items():
         if score >= v:
             return k
 
+
 sp = spacy.load('en_core_web_sm')
+
+
 def fresScore(sentence):
     '''
     Flesch reading ease score 的含义
@@ -79,10 +85,16 @@ def fresScore(sentence):
             punct = punct + 1
             continue
         total_syl = total_syl + cmusyl(sen[i].text)[0]
-    score = 206.835 - 1.015 * ((len(sen)-punct) / len(sent_tokenize(sentence))) - 84.6 * (total_syl / (len(sen)-punct))
+    score = 206.835 - 1.015 * ((len(sen) - punct) / len(sent_tokenize(sentence))) - 84.6 * (
+            total_syl / (len(sen) - punct))
     # result = {'No. of sentences':len(sent_tokenize(sentence)),
     #         'No. of words':len(sen)-punct,
     #         'Average syllables per word':total_syl/(len(sen)-punct),
     #         'Readability score':score}
     return score
     # print(total_syl, sent_tokenize(sentence), len(sen), -punct)
+
+
+if __name__ == '__main__':
+    sentence = ' hello world i love listen the song'
+    print(sentence, fresScore(sentence))
