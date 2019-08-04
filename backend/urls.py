b6716537df7108882cddd70868278c09c393d564
fyyc <zhiyumengxiang@outlook.com>
2019-08-04 09:52:40 +0800

    update 查询优秀答案模块实现中

--- backend/urls.py
+++ backend/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # ex:/backend/problems/s/1
     path('problems/<str:problem_type>/<int:user_id>', views.get_sentence_for_user),
     # 根据id获取题目
-    path('problems/<str:problem_type>/<int:problem_index>/user/<int:user_id>', views.get_sentence_by_id,
+    path('problems/<str:problem_type>/<int:problem_index>/user/<int:user_id>', views.get_sentence_by_id),
     #提交题目并获取答案
     path('evaluation/<str:json_sentence>',views.evaluate_sentence)
 ]
