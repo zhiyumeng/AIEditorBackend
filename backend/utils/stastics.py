035c606e1b45ddc93c1facb6548e289b81f9a900
fyyc <zhiyumengxiang@outlook.com>
2019-08-06 17:50:37 +0800

    update 根据时间统计数据

--- backend/utils/stastics.py
+++ backend/utils/stastics.py
@@ -1,12 +1,15 @@
 from backend.models import ProblemRecord
 import datetime
 
-start = datetime.datetime(2019, 8, 6, 12, 0, 0, 0)
-end = datetime.datetime.now()
+from django.utils import timezone
 
 
 # 获取用户当天的题目数量
-def get_user_problem_record_num(user_id):
+def get_user_problem_record_num(user_id, start=None, end=None):
+    if start is None:
+        start = datetime.datetime.today()
+    if end is None:
+        end = datetime.date.today() + datetime.timedelta(days=1)
     query_rs = ProblemRecord.objects.filter(user_id=user_id, add_date__range=(start, end)).values('problem_id')
     ids = set([q['problem_id'] for q in query_rs])
     return len(ids)
