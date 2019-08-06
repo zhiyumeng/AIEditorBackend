456a54eed4138a8730f38483bbb613234742387c
fyyc <zhiyumengxiang@outlook.com>
2019-08-06 15:00:25 +0800

    update 同义词

--- ml_models/synonym.py
+++ ml_models/synonym.py
@@ -2,9 +2,13 @@ import sys
 sys.path.append('/data/zly')
 
 from xkcd import get_syn
-
+import time
 
 if __name__ == '__main__':
+    tic = time.time()
     sentence = 'hello i wanto to find som synonym.'
+    toc = time.time()
     print(sentence)
     print(get_syn(sentence))
+    print(toc-tic)
+
