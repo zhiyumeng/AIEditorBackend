b82de2038abac2d8870e4bce026121e3b03b2262
fyyc <zhiyumengxiang@outlook.com>
2019-08-06 20:20:27 +0800

    update 根据时间统计数据

--- backend/urls.py
+++ backend/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     # 根据题目id获取优秀答案
     path('good_answers/<int:problem_id>', views.get_good_answers),
     path('history_answers/<int:user_id>/<int:page_index>', views.get_history_answers_by_page),
-    path('statics/user/<int:user_id>', views.get_stastics)
+    path('statics/user/<int:user_id>', views.get_staistic_for_user)
 ]
