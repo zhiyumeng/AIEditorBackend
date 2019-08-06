7989ab7d77eb0c8937726e380b7d2790b3ec7870
fyyc <zhiyumengxiang@outlook.com>
2019-08-06 17:14:05 +0800

    update 统计做题数据

--- backend/utils/stastics.py
+++ backend/utils/stastics.py
@@ -7,7 +7,7 @@ end = datetime.datetime.now()
 
 # 获取用户当天的题目数量
 def get_user_problem_record_num(user_id):
-    query_rs = ProblemRecord.objects.filter(user_id=user_id, add_time__range=(start, end)).values('problem_id')
+    query_rs = ProblemRecord.objects.filter(user_id=user_id, add_date__range=(start, end)).values('problem_id')
     ids = set([q['problem_id'] for q in query_rs])
     return len(ids)
 
