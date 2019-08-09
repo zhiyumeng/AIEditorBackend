fdeccf237b82785282c753f43ecf456c7ab00817
fyyc <zhiyumengxiang@outlook.com>
2019-08-09 20:13:26 +0800

    update 旭东   paraphraser

--- ml_models/paraphraser.py
+++ ml_models/paraphraser.py
@@ -1,3 +1,10 @@
 import sys
-
+from inference import model_init,inference
 sys.path.append('/data/lxd/paraphraser')
+
+model = model_init(0.5)
+
+if __name__ == '__main__':
+    sentence = 'hello, i have a dream'
+    print(sentence)
+    print(inference(model,sentence))
