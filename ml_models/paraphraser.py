a0bddabf46ad2b201a3659e07246202dcc7e4447
fyyc <zhiyumengxiang@outlook.com>
2019-08-10 16:15:37 +0800

    update 注释paraphraser

--- ml_models/paraphraser.py
+++ ml_models/paraphraser.py
@@ -1,16 +1,17 @@
 import sys
 
 sys.path.append('/data/lxd/paraphraser')
-from inference import model_init, inference
+#from inference import model_init, inference todo:remove note
 
 
 def load_paraphraser():
+    return 'hello'
     model = model_init(0.5)
 
     def paraphraser(sentence):
         return inference(model, sentence)
 
-    return paraphraser
+    #return paraphraser todo:remove note
 
 
 if __name__ == '__main__':
