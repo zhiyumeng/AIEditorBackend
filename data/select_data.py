711a5539dc4669e41c8f6c1b1851b9a3ff2d3158
fyyc <zhiyumengxiang@outlook.com>
2019-08-13 11:39:41 +0800

    update 挑选模型

---
+++ data/select_data.py
@@ -0,0 +1,18 @@
+from ml_models.ltt_similarity import bimpmPred as ltt_serving
+from ml_models.similarity import inferencePairsFromGraph as cz_serving
+from ml_models.mying_similarity import my_similarity_serving as my_serving
+import pandas as pd
+
+
+def evaluate_setence(sent1, sent2):
+    ltt = ltt_serving(sent1, sent2)
+    my = my_serving(sent1, sent2)
+    cz = cz_serving(sent1, sent2)
+    return ltt, my, cz
+
+
+def stastics():
+    df = pd.read_csv('data/sentence_pairs_web.csv')[['question1', 'question1']]
+    rs = [evaluate_setence(sent1, sent2) for sent1, sent2 in df.values]
+    rs = zip(*rs)
+    return pd.DataFrame({'ltt': rs[0], 'my': rs[1], 'cz': rs[2]})
